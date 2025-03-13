d07d31cd4188fa6c06e064808364f3d42321a4fa
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2025-03-13 10:28:27 +0100

    Opgave 2.1 done

--- assets/js/operators.js
+++ assets/js/operators.js
@@ -1,4 +1,3 @@
-
 // variabler du skal bruge i din kode...
 const A = 10;
 const B = 30;
@@ -8,20 +7,12 @@ const E = 40;
 
 // Opgave 1: skriv et expression der viser true når A er lig med B eller A er lig med D.
 
-if ( /*..dit expression Her*/ ) {
-    console.log('statement true!');
+if (A === B || A === D) {
+  console.log('statement true!');
 }
-
 
 //opgave 2: skrive en If then statement der bliver TRUE når  A er større end B og A er mindre end E.
 
-
-
-
 //opgave 3: Skriv en If then statement med  && og  || der kun er sand når alle expressions er sande.
-
-
-
-
 
 //opgave 4: Skriv en If then statement der kan finde ud af om F er "undefined" eller har en værdi, ved at bruge !.
